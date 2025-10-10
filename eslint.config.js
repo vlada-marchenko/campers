c6558b9d0d508fb2a1f0ec4700c109be699a6f4b
vlada-marchenko <vvvvadya44@gmail.com>
2025-10-11 01:40:02 +0200

    fixess

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,6 @@
 import js from '@eslint/js';
-import globals from 'globals';
 import tseslint from 'typescript-eslint';
 import nextPlugin from '@next/eslint-plugin-next';
-import reactHooks from 'eslint-plugin-react-hooks';
 
 export default [
   { ignores: ['.next', 'node_modules', 'dist'] },
@@ -10,15 +8,4 @@ export default [
   js.configs.recommended,
   ...tseslint.configs.recommended,
   nextPlugin.configs['core-web-vitals'],
-
-  {
-    files: ['**/*.{ts,tsx,js,jsx}'],
-    languageOptions: {
-      parser: tseslint.parser,
-      parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-      globals: { ...globals.browser, ...globals.node },
-    },
-    plugins: { 'react-hooks': reactHooks },
-    rules: { ...reactHooks.configs['recommended-latest'].rules },
-  },
 ];
