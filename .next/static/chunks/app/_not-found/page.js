0898a13ca672072057061217cc0e3e57a11f0e71
vlada-marchenko <vvvvadya44@gmail.com>
2026-05-05 21:51:06 +0200

    add: header mob

--- .next/static/chunks/app/_not-found/page.js
+++ .next/static/chunks/app/_not-found/page.js
@@ -6,7 +6,7 @@
  * or disable the default devtool with "devtool: false".
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
-(self["webpackChunk_N_E"] = self["webpackChunk_N_E"] || []).push([["app/not-found"],{
+(self["webpackChunk_N_E"] = self["webpackChunk_N_E"] || []).push([["app/_not-found/page"],{
 
 /***/ "(app-pages-browser)/./app/page.module.css":
 /*!*****************************!*\
